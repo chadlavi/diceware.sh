3640d045223f89c7cc41266b0bfbab4c13d0da8a
Chad Lavimoniere <chadlavi@gmail.com>
2017-04-02 00:08:53 -0400

    add to README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# diceware.sh
+# diceware.sh  
+[Diceware](http://world.std.com/~reinhold/diceware.html) is "a better way to create a strong, yet easy to remember passphrase for use with encryption and security programs".
+This POSIX shell script is an easy way to get a robot to do the looking up and compiling for you--all you have to do is roll the die and enter your results.
+
+cf. https://en.wikipedia.org/wiki/Diceware, http://world.std.com/~reinhold/diceware.html
+
---
+++ diceware.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+(
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+count=0
+results=()
+read -n 1 -s -p "To use diceware, roll a d6 five times for each word."
+echo ""
+read -n 1 -s -p "WHEN YOU HAVE YOUR DI(C)E READY, PRESS ANY KEY"
+echo "\n\n"
+read -p "How many words do you need? (default: 6) "  words
+words=${words:-6}
+re='^[0-9]+$'
+reg='^[1-6]{5}$'
+while ! [[ $words =~ $re ]] ; do
+	read -p "Number of words must be integer: " words
+done
+while [ $count -lt $words ]; do
+count=$(($count + 1))
+read -p "List your 5 rolls for word $count as a single string (e.g., '12345'): " key
+while ! [[ $key =~ $reg ]] ; do
+	read -p "Rolls for word $count must be entered as 5 digits between 1 and 6: " key
+done
+results+=$(cat $DIR/diceware.txt | grep $key | awk '{print $2}')
+results+=" "
+done
+echo "\n\nResult:\n\n${results}\n"
+)
